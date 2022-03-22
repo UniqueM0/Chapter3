35de7fee187152fa90c3b3767be140b5c72f0666
Unique Orduna <uniqueorduna@swuser-129-8-204-181.swlan.csufresno.edu>
2022-03-22 15:39:54 -0700

    added 3_2

---
+++ 3_2.py
@@ -0,0 +1,59 @@
+
+# grade = 90
+
+# condition = grade >=90
+
+# if condition:
+#     # execute when true
+#     print("your grade is A")
+# else:
+#     # execute when false
+#     print("your grade is not A")
+
+# ## Determine the larger of two numbers
+# ## Obtain the two numbers from the user
+
+# num1 = eval(input("Enter the first number: "))
+# num2 = eval(input("Enter the second number: "))
+# try:
+
+# # Determine and display the larger value
+
+# # if num1 > num2:
+# #     larger_value = num1 #execute this statement if the condition is true
+# # else:
+# #     larger_value = num2 # execute this statement if the condition is false
+# # print("The larger value is", str(larger_value) + ".")
+
+# # print(isinstance(num1, str)) #will print false
+
+# # if isinstance(num1, str):
+# #     print("Data type not allowed: Please specify a numeric data type")
+
+
+# print("1", isinstance(num1, str))
+# print("2", isinstance(num2, str))
+
+# if isinstance(num1, str) or isinstance(num2, str):
+#     print ("Data type not allowed: please specify a numeric data")
+# else:
+#     num1 = eval(num1)
+#     num2 = eval(num2)
+#     if num1 > num2:
+#         larger_val = num1
+#     else: 
+#         larger_val = num2
+#     print("The larger value is " + str(larger_val))
+# except:
+#     print("Data is not allowed")
+
+
+# answer = eval(input("How many gallons does a ten-gallon hat hold? "))
+
+# if (0.5 <= answer <= 1):
+#     print("Good, ", end="")
+# else:
+#     print("No, ", end="")
+# print("It hold about 3/4 of a gallon.")
+
+
